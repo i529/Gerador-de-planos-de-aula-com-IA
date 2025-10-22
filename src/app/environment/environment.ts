bbc35372685b34dbeef9e834d824215a6cbc049a
Icaro Ricardo <68976113+i529@users.noreply.github.com>
2025-10-22 07:27:25 -0300

    Remove environment configuration variables

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,1 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://pjxbuyotimolqbdzdqis.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqeGJ1eW90aW1vbHFiZHpkcWlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NjY3NDMsImV4cCI6MjA3NjU0Mjc0M30.7cmIGS9vb0TAkKCfHmdzPsTbL3tMuGGNMi7Rme0XZf4',
-  geminiApiKey: 'AIzaSyCgyqzoGZXBUGoPL-MqF9UcE3S8o2XD4ss'
-};
+
